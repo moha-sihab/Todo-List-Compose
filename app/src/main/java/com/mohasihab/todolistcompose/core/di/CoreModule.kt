1c0e3346172b5b6f14b73c5a77d81d6d2fb9bd88
moha-sihab <moha.sihab@gmail.com>
2023-07-30 06:15:03 +0700

    Setup hilt core

---
+++ app/src/main/java/com/mohasihab/todolistcompose/core/di/CoreModule.kt
@@ -0,0 +1,43 @@
+package com.mohasihab.todolistcompose.core.di
+
+import android.content.Context
+import androidx.room.Room
+import com.mohasihab.todolistcompose.core.data.local.TodoTaskDao
+import com.mohasihab.todolistcompose.core.data.local.TodoTaskDatabase
+import com.mohasihab.todolistcompose.core.data.repositories.TodoTaskRepository
+import com.mohasihab.todolistcompose.core.data.repositories.TodoTaskRepositoryContract
+import com.mohasihab.todolistcompose.core.domain.usecase.TodoTaskUseCase
+import com.mohasihab.todolistcompose.core.domain.usecase.TodoTaskUseCaseContract
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoreModule {
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context,
+    ) = Room.databaseBuilder(
+        context,
+        TodoTaskDatabase::class.java,
+        "tododatabase"
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: TodoTaskDatabase) = database.todoTaskDao()
+
+    @Singleton
+    @Provides
+    fun provideRepository(dao: TodoTaskDao): TodoTaskRepositoryContract = TodoTaskRepository(dao)
+
+    @Singleton
+    @Provides
+    fun provideUseCase(repository: TodoTaskRepositoryContract): TodoTaskUseCaseContract =
+        TodoTaskUseCase(repository)
+}
