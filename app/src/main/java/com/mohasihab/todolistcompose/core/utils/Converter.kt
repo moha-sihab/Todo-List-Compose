b6786cf85485c672af602db51152582b39bb5549
moha-sihab <moha.sihab@gmail.com>
2023-07-30 06:16:48 +0700

    Fix issue converter date on Room Dabatase

---
+++ app/src/main/java/com/mohasihab/todolistcompose/core/utils/Converter.kt
@@ -0,0 +1,37 @@
+package com.mohasihab.todolistcompose.core.utils
+
+import androidx.room.TypeConverter
+import java.math.BigDecimal
+import java.util.Date
+
+class Converter {
+    companion object{
+        @TypeConverter
+        @JvmStatic
+        fun fromBigDecimal(value: BigDecimal):String{
+            return value.toString()
+        }
+
+        @TypeConverter
+        @JvmStatic
+        fun toBigDecimal(value:String):BigDecimal{
+            return value.toBigDecimal()
+        }
+
+        @TypeConverter
+        @JvmStatic
+        fun toDate(value:Long): Date?{
+
+            var resultDate : Date? = null
+            resultDate=  if (value == null) null else Date(value)
+            return resultDate
+        }
+
+        @TypeConverter
+        @JvmStatic
+        fun fromDate(date:Date): Long {
+            return date.time
+        }
+
+    }
+}
